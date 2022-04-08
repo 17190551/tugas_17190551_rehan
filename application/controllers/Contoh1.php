d73ada1bc603220de3068a97c636d02d6a053cf3
17190551 <17190551@bsi.ac.id>
2022-04-09 03:02:35 +0700

    update

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo"<h1>Welcome to my blog </h1>";
-        echo"Nama saya Rehan H
+        echo"Nama saya Rehan Hardiansyah
             Saya tinggal di daerah Bogor
             olah raga yang saya sukai adalah
             Futsal";
